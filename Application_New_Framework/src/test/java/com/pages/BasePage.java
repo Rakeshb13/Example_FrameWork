84626db2914c758d4f0160f09934a5b69a932841
Sam Smith <sam@example.com>
2024-11-12 16:02:44 +0530

    New changes

--- Application_New_Framework/src/test/java/com/pages/BasePage.java
+++ Application_New_Framework/src/test/java/com/pages/BasePage.java
@@ -35,9 +35,6 @@ public class BasePage {
 	public static ExtentReports report;
 	public static ExtentTest test;
 	
-	Select select;
-	Actions action;
-
 	@BeforeSuite
 	public void setUpReport() {
 		driver_Setup.configureReport();
@@ -49,166 +46,13 @@ public class BasePage {
 		driver=driver_Setup.invokeBrowser(browserName);
 	}
 	
-	
 	@AfterClass
 	public void closeBrowser() {
 		driver_Setup.quitBrowser();
 	}
 	
-	
 	@AfterSuite
 	public void tearDownReport() {
 		driver_Setup.finalizeReport();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public void selectDropDownByValue(WebElement dropdown, String value) {
-		select = new Select(dropdown);
-		select.selectByValue(value);
-	}
-
-	public void selectDropDownByVisibleText(WebElement dropdown, String visibleText) {
-		select = new Select(dropdown);
-		select.selectByVisibleText(visibleText);
-	}
-
-	
-	public boolean verifyIsDropDownIsMultiSelect(WebElement dropDown) {
-		select = new Select(dropDown);
-		return select.isMultiple();
-	}
-
-	
-	public void deselectAllOptionsFromDropDown(WebElement dropDown) {
-		if (verifyIsDropDownIsMultiSelect(dropDown)) {
-			select = new Select(dropDown);
-			select.deselectAll();
-		} else
-			System.out.println("Can't De-select Options from an Single Select Dropdown");
-	}
-
-	
-	public void deselectDropDownByValue(WebElement dropdown, String value) {
-		select = new Select(dropdown);
-		select.deselectByVisibleText(value);
-	}
-
-	
-	public void deselectDropDownByVisibleText(WebElement dropdown, String visibleText) {
-		select = new Select(dropdown);
-		select.deselectByVisibleText(visibleText);
-	}
-
-	
-	public void acceptAlert(WebDriver driver) {
-		driver.switchTo().alert().accept();
-	}
-
-	
-	public void dismissAlert(WebDriver driver) {
-		driver.switchTo().alert().dismiss();
-	}
-
-	
-	public String getAlertText(WebDriver driver) {
-		return driver.switchTo().alert().getText();
-	}
-
-	
-	public void passValueToAlert(WebDriver driver, String value) {
-		driver.switchTo().alert().sendKeys(value);
-	}
-	
-	
-	public void switchToFrame(WebDriver driver, int index) {
-		driver.switchTo().frame(index);
-	}
-	
-	
-	public void switchtoFrame(WebDriver driver, String idOrName) {
-		driver.switchTo().frame(idOrName);
-	}
-	
-	
-	public void switchToFrame(WebDriver driver, WebElement frameElement) {
-		driver.switchTo().frame(frameElement);
-	}
-	
-	
-	public void switchToWindow(WebDriver driver, String pageTitle) {
-
-		Set<String> all_Session_Ids = driver.getWindowHandles();
-
-		for (String session_Id : all_Session_Ids) {
-			driver.switchTo().window(session_Id);
-			if (driver.getTitle().equals(pageTitle)) {
-				break;
-			}
-		}
-	}
-
-	
-	public void captureScreenshot(WebDriver driver, String screenshotTitle) throws IOException {
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		File src = ts.getScreenshotAs(OutputType.FILE);
-		File trg = new File(FrameworkConstants.SCREENSHOT_PATH + screenshotTitle + ".png");
-		FileHandler.copy(src, trg);
-	}
-	
-	
-	public String captureScreenshotForReport(WebDriver driver) {
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		return ts.getScreenshotAs(OutputType.BASE64);
-	}
-	
-	
-	public void waitUntilVisibilityOfElement(WebDriver driver, WebElement element) {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.visibilityOf(element));
-	}
-	
-	
-	public void waitUntilInVisibilityOfElement(WebDriver driver, WebElement element) {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.invisibilityOf(element));
-	}
-
-	public void waitUntilPresenceOfElement(WebDriver driver, By locator) {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-	}
-	
-	
-	public void mouseHoverOnElement(WebDriver driver,WebElement targetElement) {
-		action = new Actions(driver);
-		action.moveToElement(targetElement).perform();
-	}
-	
-	
-	public void clickOnElement(WebDriver driver, WebElement targetElement) {
-		action = new Actions(driver);
-		action.click(targetElement).perform();
-	}
-	
-	public void actionsScrollTillElement(WebDriver driver, WebElement targetElement) {
-		action = new Actions(driver);
-		action.scrollToElement(targetElement).perform();
-	}
-	
 }
